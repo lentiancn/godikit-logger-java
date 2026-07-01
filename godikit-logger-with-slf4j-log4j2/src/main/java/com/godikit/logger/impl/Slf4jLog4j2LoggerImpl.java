e4d60914faf0053419faeeaf8fbb2b69b78d644d
Len <lentiancn@126.com>
2026-07-01 22:51:55 +0800

    docs(logger): add comprehensive javadoc documentation for slf4j logger implementations
    - Add detailed class-level documentation for Slf4jLog4j2LoggerImpl
    - Add detailed class-level documentation for Slf4jLogbackLoggerImpl
    - Document FACADE, PROVIDER, and FACADE_LOGGER constants with purpose descriptions
    - Add parameter and return value documentation for all constructor methods
    - Add comprehensive documentation for all interface method implementations
    - Document all logging level methods (trace, debug, info, warn, error) with parameters
    - Add documentation for exception handling methods with throwable parameters
    - Include proper @param and @return tags for all documented methods
    - Add descriptive comments explaining the purpose of each logger implementation

--- godikit-logger-with-slf4j-log4j2/src/main/java/com/godikit/logger/impl/Slf4jLog4j2LoggerImpl.java
+++ godikit-logger-with-slf4j-log4j2/src/main/java/com/godikit/logger/impl/Slf4jLog4j2LoggerImpl.java
@@ -45,9 +45,19 @@ import static com.godikit.logger.utils.LoggerUtils.mergeMsgAndThrowable;
  */
 public class Slf4jLog4j2LoggerImpl implements Logger {
 
+    /**
+     * The facade identifier for this logger implementation.
+     */
     public static final String FACADE = "slf4j";
+
+    /**
+     * The provider identifier for this logger implementation.
+     */
     public static final String PROVIDER = "log4j2";
 
+    /**
+     * The underlying SLF4J Logger instance used for actual logging operations.
+     */
     private final org.slf4j.Logger FACADE_LOGGER;
 
     /**
@@ -68,120 +78,255 @@ public class Slf4jLog4j2LoggerImpl implements Logger {
         FACADE_LOGGER = org.slf4j.LoggerFactory.getLogger(clazz);
     }
 
-    public Slf4jLog4j2LoggerImpl(final org.slf4j.Logger logger) {
-        FACADE_LOGGER = logger;
+    /**
+     * Creates a logger from an existing SLF4J Logger instance.
+     *
+     * @param facadeLogger the SLF4J Logger instance to wrap
+     */
+    public Slf4jLog4j2LoggerImpl(final org.slf4j.Logger facadeLogger) {
+        FACADE_LOGGER = facadeLogger;
     }
 
+    /**
+     * Returns the facade name identifier.
+     *
+     * @return the facade identifier, always "slf4j"
+     */
     @Override
     public String getFacadeName() {
         return FACADE;
     }
 
+    /**
+     * Returns the underlying SLF4J Logger instance.
+     *
+     * @return the wrapped {@code org.slf4j.Logger} instance
+     */
     @Override
     public Object getFacadeLogger() {
         return FACADE_LOGGER;
     }
 
+    /**
+     * Returns the provider name identifier.
+     *
+     * @return the provider identifier, always "log4j2"
+     */
     @Override
     public String getProviderName() {
         return PROVIDER;
     }
 
+    /**
+     * Checks if TRACE level logging is enabled.
+     *
+     * @return true if TRACE level is enabled in the underlying logger
+     */
     @Override
     public boolean isTraceEnabled() {
         return FACADE_LOGGER.isTraceEnabled();
     }
 
+    /**
+     * Checks if DEBUG level logging is enabled.
+     *
+     * @return true if DEBUG level is enabled in the underlying logger
+     */
     @Override
     public boolean isDebugEnabled() {
         return FACADE_LOGGER.isDebugEnabled();
     }
 
+    /**
+     * Checks if INFO level logging is enabled.
+     *
+     * @return true if INFO level is enabled in the underlying logger
+     */
     @Override
     public boolean isInfoEnabled() {
         return FACADE_LOGGER.isInfoEnabled();
     }
 
+    /**
+     * Checks if WARN level logging is enabled.
+     *
+     * @return true if WARN level is enabled in the underlying logger
+     */
     @Override
     public boolean isWarnEnabled() {
         return FACADE_LOGGER.isWarnEnabled();
     }
 
+    /**
+     * Checks if ERROR level logging is enabled.
+     *
+     * @return true if ERROR level is enabled in the underlying logger
+     */
     @Override
     public boolean isErrorEnabled() {
         return FACADE_LOGGER.isErrorEnabled();
     }
 
+    /**
+     * Logs a message at TRACE level with optional arguments.
+     *
+     * @param msg  the message pattern
+     * @param args the arguments to be substituted into the message pattern
+     */
     @Override
     public void trace(final String msg, final Object... args) {
         FACADE_LOGGER.trace(msg, args);
     }
 
+    /**
+     * Logs an exception at TRACE level.
+     *
+     * @param cause the Throwable to log with its full stack trace
+     */
     @Override
     public void trace(final Throwable cause) {
         FACADE_LOGGER.trace(LoggerThrowableUtils.toString(cause));
     }
 
+    /**
+     * Logs a message at TRACE level with an exception and optional arguments.
+     *
+     * @param cause the Throwable to log
+     * @param msg   the message pattern
+     * @param args  the arguments to be substituted into the message pattern
+     */
     @Override
     public void trace(final Throwable cause, final String msg, final Object... args) {
         FACADE_LOGGER.trace(mergeMsgAndThrowable(msg, cause), args);
     }
 
+    /**
+     * Logs a message at DEBUG level with optional arguments.
+     *
+     * @param msg  the message pattern
+     * @param args the arguments to be substituted into the message pattern
+     */
     @Override
     public void debug(final String msg, final Object... args) {
         FACADE_LOGGER.debug(msg, args);
     }
 
+    /**
+     * Logs an exception at DEBUG level.
+     *
+     * @param cause the Throwable to log with its full stack trace
+     */
     @Override
     public void debug(final Throwable cause) {
         FACADE_LOGGER.debug(LoggerThrowableUtils.toString(cause));
     }
 
+    /**
+     * Logs a message at DEBUG level with an exception and optional arguments.
+     *
+     * @param cause the Throwable to log
+     * @param msg   the message pattern
+     * @param args  the arguments to be substituted into the message pattern
+     */
     @Override
     public void debug(final Throwable cause, final String msg, final Object... args) {
         FACADE_LOGGER.debug(mergeMsgAndThrowable(msg, cause), args);
     }
 
+    /**
+     * Logs a message at INFO level with optional arguments.
+     *
+     * @param msg  the message pattern
+     * @param args the arguments to be substituted into the message pattern
+     */
     @Override
     public void info(final String msg, final Object... args) {
         FACADE_LOGGER.info(msg, args);
     }
 
+    /**
+     * Logs an exception at INFO level.
+     *
+     * @param cause the Throwable to log with its full stack trace
+     */
     @Override
     public void info(final Throwable cause) {
         FACADE_LOGGER.info(LoggerThrowableUtils.toString(cause));
     }
 
+    /**
+     * Logs a message at INFO level with an exception and optional arguments.
+     *
+     * @param cause the Throwable to log
+     * @param msg   the message pattern
+     * @param args  the arguments to be substituted into the message pattern
+     */
     @Override
     public void info(final Throwable cause, final String msg, final Object... args) {
         FACADE_LOGGER.info(mergeMsgAndThrowable(msg, cause), args);
     }
 
+    /**
+     * Logs a message at WARN level with optional arguments.
+     *
+     * @param msg  the message pattern
+     * @param args the arguments to be substituted into the message pattern
+     */
     @Override
     public void warn(final String msg, final Object... args) {
         FACADE_LOGGER.warn(msg, args);
     }
 
+    /**
+     * Logs an exception at WARN level.
+     *
+     * @param cause the Throwable to log with its full stack trace
+     */
     @Override
     public void warn(final Throwable cause) {
         FACADE_LOGGER.warn(LoggerThrowableUtils.toString(cause));
     }
 
+    /**
+     * Logs a message at WARN level with an exception and optional arguments.
+     *
+     * @param cause the Throwable to log
+     * @param msg   the message pattern
+     * @param args  the arguments to be substituted into the message pattern
+     */
     @Override
     public void warn(final Throwable cause, final String msg, final Object... args) {
         FACADE_LOGGER.warn(mergeMsgAndThrowable(msg, cause), args);
     }
 
+    /**
+     * Logs a message at ERROR level with optional arguments.
+     *
+     * @param msg  the message pattern
+     * @param args the arguments to be substituted into the message pattern
+     */
     @Override
     public void error(final String msg, final Object... args) {
         FACADE_LOGGER.error(msg, args);
     }
 
+    /**
+     * Logs an exception at ERROR level.
+     *
+     * @param cause the Throwable to log with its full stack trace
+     */
     @Override
     public void error(final Throwable cause) {
         FACADE_LOGGER.error(LoggerThrowableUtils.toString(cause));
     }
 
+    /**
+     * Logs a message at ERROR level with an exception and optional arguments.
+     *
+     * @param cause the Throwable to log
+     * @param msg   the message pattern
+     * @param args  the arguments to be substituted into the message pattern
+     */
     @Override
     public void error(final Throwable cause, final String msg, final Object... args) {
         FACADE_LOGGER.error(mergeMsgAndThrowable(msg, cause), args);
